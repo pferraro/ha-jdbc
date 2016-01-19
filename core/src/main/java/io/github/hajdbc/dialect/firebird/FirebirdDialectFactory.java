f8d0c0d67368239a51eb21f8f456048b877fe415
Paul Ferraro <paul.ferraro@redhat.com>
2016-01-19 14:50:07 -0500

    Refactor net.sf -> io.github

---
+++ core/src/main/java/io/github/hajdbc/dialect/firebird/FirebirdDialectFactory.java
@@ -0,0 +1,38 @@
+/*
+ * HA-JDBC: High-Availability JDBC
+ * Copyright (C) 2012  Paul Ferraro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.github.hajdbc.dialect.firebird;
+
+import io.github.hajdbc.dialect.Dialect;
+import io.github.hajdbc.dialect.DialectFactory;
+
+public class FirebirdDialectFactory implements DialectFactory
+{
+	private static final long serialVersionUID = 8753385208104801124L;
+
+	@Override
+	public String getId()
+	{
+		return "firebird";
+	}
+
+	@Override
+	public Dialect createDialect()
+	{
+		return new FirebirdDialect();
+	}
+}
